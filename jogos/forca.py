9c7cd79c4266d640d8363374e037f64fab2f2d34
Bruno Rocha <bruno-ralmeida@outlook.com>
2020-03-29 23:39:08 -0300

    Jogo Forca - Alura

--- jogos/forca.py
+++ jogos/forca.py
@@ -3,20 +3,38 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_sec = 'batata'
-    enforcou = False
-    acertou = False
-    palavra_chute = []
+    palavra_sec = 'batata'.upper()
+    enforcou = acertou = False
+    lst_acerto = ['_' for letra in palavra_sec] #List Comprehension
+    qtd_chute = len(palavra_sec) + 2
 
     while(not enforcou and not acertou):
-        chute = input('Informe uma letra: ')
+        
+        if(qtd_chute != 0):
 
-        if(palavra_sec.__contains__(chute) and not palavra_chute.__contains__(chute)):
+            chute = str(input('\nInforme uma letra: ')).strip().upper()
+            index = 0
+            qtd_chute -= 1
 
-            palavra_chute.append(chute)
-            print(palavra_chute)
+            for letra in palavra_sec:
+                if(chute == letra):
+                    #print(f'Encontrei a lertra {chute} na posição {index+1}.')
+                    lst_acerto[index] = letra
+                    acertou = '_' not in lst_acerto 
+                index += 1
 
-    print("Fim do jogo")
+            #Mostando as letras acertadas
+            for i,v in enumerate(lst_acerto):
+                if(i != len(lst_acerto)):
+                    print(f'{v} ', end='')
+                                
+        else:
+            print('\n\nEnforcou!')
+            enforcou = True
+        
+                
+            
+    print("\nFim do jogo")
 
 if(__name__ == "__main__"):
     jogar()
