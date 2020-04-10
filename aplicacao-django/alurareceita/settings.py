a720be3ae59da61c0ae25dbba51ad8f2ae72041d
Bruno Rocha <bruno-ralmeida@outlook.com>
2020-04-10 17:27:34 -0300

    Conectando com Postrgesql.

--- aplicacao-django/alurareceita/settings.py
+++ aplicacao-django/alurareceita/settings.py
@@ -59,8 +59,11 @@ WSGI_APPLICATION = 'alurareceita.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'alura_receitas',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost'
     }
 }
 
--- aplicacao-django/receitas/views.py
+++ aplicacao-django/receitas/views.py
@@ -2,7 +2,16 @@ from django.shortcuts import render
 
 #Função render, devemos passar o request como parametro.
 def index(request):
-    return render(request,'index.html')
+    receitas = {
+        1: 'Lasanha',
+        2: 'Sopa de Legumes',
+        3: 'Sorvete',
+        4: 'Bolo de chocolate'
+    }
+    dados = {
+        'nome_receitas': receitas
+    }
+    return render(request,'index.html',dados)
 
 def receita(request):
     return render(request, 'receita.html')
