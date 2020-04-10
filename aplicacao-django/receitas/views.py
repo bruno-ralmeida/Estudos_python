7f07c197e2dbd426a228678d8df10974432009f6
Bruno Rocha <bruno-ralmeida@outlook.com>
2020-04-10 16:18:39 -0300

    Adicionando a pagina de receita e partials.

--- aplicacao-django/receitas/views.py
+++ aplicacao-django/receitas/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 #Função render, devemos passar o request como parametro.
 def index(request):
     return render(request,'index.html')
+
+def receita(request):
+    return render(request, 'receita.html')
