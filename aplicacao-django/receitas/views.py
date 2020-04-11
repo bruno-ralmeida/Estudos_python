5a9ad7da34c27464be0a552a9047e44937dc7ec6
Bruno Rocha <bruno-ralmeida@outlook.com>
2020-04-11 13:45:22 -0300

    Ajustando a tela de receita.

--- aplicacao-django/receitas/views.py
+++ aplicacao-django/receitas/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from .models import Receita
+
 #Função render, devemos passar o request como parametro.
 def index(request):
     receitas = Receita.objects.all()
@@ -8,5 +9,10 @@ def index(request):
     }
     return render(request,'index.html',dados)
 
-def receita(request):
-    return render(request, 'receita.html')
+
+def receita(request, receita_id):
+    receita_exibir = get_object_or_404(Receita, pk=receita_id)
+    receita = {
+        'receita': receita_exibir
+    }
+    return render(request, 'receita.html', receita)
