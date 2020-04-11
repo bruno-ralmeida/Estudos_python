705a9823d9e91fc54ba770ed1d1d3ab41421f881
Bruno Rocha <bruno-ralmeida@outlook.com>
2020-04-11 01:04:52 -0300

    Retornando dados do banco.

--- aplicacao-django/receitas/views.py
+++ aplicacao-django/receitas/views.py
@@ -1,15 +1,10 @@
 from django.shortcuts import render
-
+from .models import Receita
 #Função render, devemos passar o request como parametro.
 def index(request):
-    receitas = {
-        1: 'Lasanha',
-        2: 'Sopa de Legumes',
-        3: 'Sorvete',
-        4: 'Bolo de chocolate'
-    }
+    receitas = Receita.objects.all()
     dados = {
-        'nome_receitas': receitas
+        'receitas': receitas
     }
     return render(request,'index.html',dados)
 
