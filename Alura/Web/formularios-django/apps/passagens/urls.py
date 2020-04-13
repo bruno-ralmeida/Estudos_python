3d4ec635078f331a7e38ce2b4abaaa57514aa4f3
Bruno Rocha <bruno-ralmeida@outlook.com>
2020-04-13 12:50:03 -0300

    Criando função para recuperar dados.

--- Alura/Web/formularios-django/apps/passagens/urls.py
+++ Alura/Web/formularios-django/apps/passagens/urls.py
@@ -3,4 +3,5 @@ from .views import passagem
 
 urlpatterns = [
     path('', passagem.index , name='index'),
+    path('minha_consulta', passagem.revisao_consulta, name='minha_consulta'),
 ]
--- Alura/Web/formularios-django/apps/passagens/views/passagem.py
+++ Alura/Web/formularios-django/apps/passagens/views/passagem.py
@@ -6,3 +6,9 @@ def index(request):
     form = PassagemForms()
     contexto = {'form':form}
     return render(request, 'index.html', contexto)
+
+def revisao_consulta(request):
+    if request.method == 'POST':
+        form = PassagemForms(request.POST) #Recuperando todas informações do formulário
+        contexto = {'form':form} #Inserindo no contexto para ser renderizado no HTML
+    return render(request, 'minha_consulta.html', contexto)
